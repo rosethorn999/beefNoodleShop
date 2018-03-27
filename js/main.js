4c9271093a4c14974fbd5c56272cf8b6a81d0450
rosethorn999 <24700793+rosethorn999@users.noreply.github.com>
2018-03-27 20:33:48 +0800

    Add slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-$(function() {
-    $(".content").text( "The DOM is now loaded and can be manipulated." );
+$(function () {
+  //initialize swiper when document ready
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    setWrapperSize: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
+});
