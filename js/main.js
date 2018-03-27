26dff59acf5df34e6d2a538e97bb7904a1220b3d
rosethorn999 <24700793+rosethorn999@users.noreply.github.com>
2018-03-27 21:06:58 +0800

    Add scrollToTop button

--- js/main.js
+++ js/main.js
@@ -13,4 +13,20 @@ $(function () {
       prevEl: '.swiper-button-prev',
     },
   });
+
+
+  window.addEventListener("scroll", function () {
+    var scrollHeight = document.documentElement.scrollTop;
+    var ele = document.querySelector(".backToTop");
+    if (scrollHeight > 100) {
+      ele.style.display = 'inline'; // Show
+    } else {
+      ele.style.display = 'none'; // Hide
+    }
+  });
+
+  var backToTopButton = document.querySelector(".backToTop");
+  backToTopButton.addEventListener("click", function () {
+    window.scrollTo(0, 0);
+  }, false);
 });
